f990e44badcc3e0a14478088fcda5f776d3323fd
Luca10992 <lucalillo.99.1@gmail.com>
2024-03-16 17:03:02 +0100

    fix. edit grid

--- resources/views/comicsPage.blade.php
+++ resources/views/comicsPage.blade.php
@@ -3,8 +3,8 @@
 @section('main-content')
 
 <div class="comics">
-    <div class="container">
-        <div class="row">
+    <div class="container pt-5 pb-4">
+        <div class="row g-4">
             @foreach ($comics as $comic)
                 <div class="col-2">
                     <div class="card">
@@ -13,6 +13,9 @@
                     </div>
                 </div>
             @endforeach
+        </div>
+        <div class="text-center">
+            <button class="btn btn-primary">LOAD MORE</button>
         </div>
     </div>
 </div>
